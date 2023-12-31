ade87266b93104d56f59eb88b320c390edebc0f3
Andrey Ustinov <austinov@gmail.com>
2023-12-31 14:13:04 -0800

    fix: Add version

--- spice-agent.rb
+++ spice-agent.rb
@@ -6,6 +6,7 @@ class SpiceAgent < Formula
   homepage "https://github.com/44/spice"
   url "https://github.com/44/spice.git"
   license "MIT"
+  version "0.0.1"
 
   def install
     bin.install "spice-agent"
