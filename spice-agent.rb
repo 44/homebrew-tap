65121303815ef97ddd48a14e6acf3d169ca4182f
Andrey Ustinov <austinov@gmail.com>
2023-12-31 14:24:35 -0800

    chore: Try symlink

--- spice-agent.rb
+++ spice-agent.rb
@@ -6,11 +6,11 @@ class SpiceAgent < Formula
   homepage "https://github.com/44/spice"
   url "https://github.com/44/spice.git", branch: "main"
   license "MIT"
-  version "0.0.3"
+  version "0.0.4"
 
   def install
     libexec.install Dir["agent"]
-    bin.install "agent/spice-agent"
+    bin.install_symlink libexec/"agent/spice-agent" => "spice-agent"
   end
 
 end
